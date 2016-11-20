ab9db45cf7f02b49d1337df7bcc9c6ea12182437
Fynov <praznik.bor@gmail.com>
2016-11-20 12:37:31 +0100

    Od mikija merged
    Zdruzeno od mikija in moje. manualno

--- Reaching The Limit/Assets/Main Game/Scripts/User_movement.cs
+++ Reaching The Limit/Assets/Main Game/Scripts/User_movement.cs
@@ -14,6 +14,8 @@ public class User_movement : MonoBehaviour {
     public float jumpForce = 700f;
     int doubleJump = 0;
     Animator anim;
+    int dead = 0;
+    int amountOfAdditionalJumps = 0;
 	// Use this for initialization
 	void Start () {
 
@@ -23,46 +25,60 @@ public class User_movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
-        grounded = Physics2D.OverlapCircle(groundcheck.position, groundRadious, whatIsGround);
-        anim.SetBool("Ground", grounded);
-        anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
-
-
-
-        float move = Input.GetAxis("Horizontal");
-
-        anim.SetFloat("Speed", Mathf.Abs(move));
-
-        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
-        if(move > 0 && !facingRight)
+        if (dead == 0)
         {
-            flip();
-        }
-        else if( move < 0 && facingRight)
-        {
-            flip();
+            grounded = Physics2D.OverlapCircle(groundcheck.position, groundRadious, whatIsGround);
+            anim.SetBool("Ground", grounded);
+            anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+
+
+
+            float move = Input.GetAxis("Horizontal");
+
+            anim.SetFloat("Speed", Mathf.Abs(move));
+
+            GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+            if (move > 0 && !facingRight)
+            {
+                flip();
+            }
+            else if (move < 0 && facingRight)
+            {
+                flip();
+            }
         }
 	}
 
     void Update()
     {
-        if (grounded && Input.GetKeyDown(KeyCode.UpArrow))
+        test_setting();
+        if (dead == 0)
         {
-            anim.SetBool("Ground", false);
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+            //Skakanje
+            if (grounded && Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                anim.SetBool("Ground", false);
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+            }
+            if (!grounded && Input.GetKeyDown(KeyCode.UpArrow) && doubleJump < amountOfAdditionalJumps)
+            {
+                anim.SetBool("Ground", false);
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+                doubleJump++;
+            }
+            if (grounded)
+            {
+                doubleJump = 0;
+            }
         }
-        if (!grounded && Input.GetKeyDown(KeyCode.UpArrow) && doubleJump < 4)
+        if (dead == 1)
         {
-            anim.SetBool("Ground", false);
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
-            doubleJump++;
+            Vector3 theScale = transform.localScale;
+            theScale.y *= -1;
+            transform.localScale = theScale;
         }
-        if (grounded)
-        {
-            doubleJump = 0;
-        }
+
     }
 
    /* void doubleJump()
@@ -90,5 +106,63 @@ public class User_movement : MonoBehaviour {
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+
+    //spike hit
+    void OnCollisionEnter2D(Collision2D other)
+        {
+        if (other.gameObject.tag == "Spike")
+        {               // < ---Write whatever you want.make sure that you object that you collide with has the same tag.
+            dead++;
+           
+           // GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+
+    //void OnCollisionStay(Collision collisionInfo)
+    //{
+    //    print(gameObject.name + " and " + collisionInfo.collider.name + " are still colliding");
+    //}
+
+    //void OnCollisionExit(Collision collisionInfo)
+    //{
+    //    print(gameObject.name + " and " + collisionInfo.collider.name + " are no longer colliding");
+    //}
+    void test_setting()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            float lvlJumpForce = jumpForce;
+            float lvlMaxSpeed = maxSpeed;
+            Application.LoadLevel("RTL");
+            jumpForce = lvlJumpForce;
+            maxSpeed = lvlMaxSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            maxSpeed = maxSpeed + 10f;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            maxSpeed = maxSpeed - 10f;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            jumpForce = jumpForce + 200f;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            jumpForce = jumpForce - 200f;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            amountOfAdditionalJumps++;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            if(doubleJump != 1)
+            amountOfAdditionalJumps--;
+        }
     }
 }
---
+++ Reaching The Limit/Assets/Main Game/Scripts/collider_script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collider_script : MonoBehaviour {
+
+    Animator jumpo;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("DELA");
+        jumpo = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        Debug.Log("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+        //print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
+        //print("Their relative velocity is " + collisionInfo.relativeVelocity);
+        jumpo.SetTrigger("green_Pad-true");
+        collisionInfo.collider.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 4000));
+        jumpo.SetTrigger("green_Pad-false");
+    }
+}
