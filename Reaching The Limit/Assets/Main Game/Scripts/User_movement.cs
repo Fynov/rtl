622e94a720f5cf4462b23b9c18e47359429656d1
Fynov <praznik.bor@gmail.com>
2016-11-27 16:31:52 +0100

    Music toggle/shortcuts

--- Reaching The Limit/Assets/Main Game/Scripts/User_movement.cs
+++ Reaching The Limit/Assets/Main Game/Scripts/User_movement.cs
@@ -152,9 +152,6 @@ public class User_movement : MonoBehaviour {
             jumpForce = lvlJumpForce;
             maxSpeed = lvlMaxSpeed;
         }
-		if (Input.GetKeyDown(KeyCode.P)){
-			Application.LoadLevel(0);
-		}
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             maxSpeed = maxSpeed + 10f;
@@ -189,7 +186,15 @@ public class User_movement : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.M))
         {
             Application.LoadLevel(0);
-        }
+		}
+		if (Input.GetKeyDown(KeyCode.T)){
+			if (AudioListener.volume == 0) {
+				AudioListener.volume = 0.5f;
+
+			} else {
+				AudioListener.volume = 0f;
+			}
+		}
     }
 
     void IzpisiText()
--- Reaching The Limit/Assets/Menu/Scripts/ChangeVolume.cs
+++ Reaching The Limit/Assets/Menu/Scripts/ChangeVolume.cs
@@ -6,6 +6,7 @@ using System.Collections;
 
 public class ChangeVolume : MonoBehaviour {
 
+
 	void Awake()
 	{
 		volumeSlider.value = AudioListener.volume;
@@ -15,5 +16,17 @@ public class ChangeVolume : MonoBehaviour {
 	public AudioSource volumeAudio;
 	public void VolumeController(){
 		AudioListener.volume = volumeSlider.value;
+	}
+
+	public void MuteAudio(){
+		if (AudioListener.volume == 0) {
+			AudioListener.volume = 0.5f;
+			volumeSlider.value = AudioListener.volume;
+
+		} else {
+			AudioListener.volume = 0f;
+			volumeSlider.value = AudioListener.volume;
+		}
+		
 	}
 }
