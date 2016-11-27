8dc69521ef5d79a6d6a9709e249d21cceca43a06
Fynov <praznik.bor@gmail.com>
2016-11-27 18:53:24 +0100

    music mk2

---
+++ Reaching The Limit/Assets/Main Game/Scripts/Music.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+	static bool AudioBegin = false; 
+	void Awake()
+	{
+		if (!AudioBegin) {
+			GetComponent<AudioSource>().Play ();
+			DontDestroyOnLoad (gameObject);
+			AudioBegin = true;
+		} 
+	}
+	void Update () {
+
+		/*
+		if(Application.loadedLevelName == "Options")
+		{
+			GetComponent<AudioSource>().Stop();
+			AudioBegin = false;
+		}
+		za izklopit ce zelimo musko menjat
+		*/ 
+	}
+}
