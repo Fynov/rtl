aa94f61b0e2fc58d8d29e2ad9d241f2fe5757424
aljaz96 <mikikovacic@gmail.com>
2016-12-10 03:58:37 +0100

    New lvl in workd, undeground_1 done
    That

---
+++ Reaching The Limit/Assets/Main Game/Scripts/Warp scripts/Ug1_bonus_tree_warp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ug1_bonus_tree_warp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        collisionInfo.collider.GetComponent<Rigidbody2D>().transform.position = new Vector3(23, 350, 0);
+    }
+}
